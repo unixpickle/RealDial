fcd8fd27a29990026c9f728b7d720f737474fb9e
Alex Nichol <alexnichol@comcast.net>
2013-03-03 09:44:28 -0500

    Initial Commit

---
+++ RealDial/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  RealDial
+//
+//  Created by Alex Nichol on 3/3/13.
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
